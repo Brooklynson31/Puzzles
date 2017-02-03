438542f0d132abb0492a3fd6d45744ee9d3f3575
Brooklynson31 <mikeharp212@gmail.com>
2017-02-03 13:32:54 -0500

    update

---
+++ EasyProblems/src/leetcode/binarysearch/GuessGame.java
@@ -0,0 +1,40 @@
+package leetcode.binarysearch;
+
+public class GuessGame {
+	
+//		We are playing the Guess Game. The game is as follows:
+//
+//		I pick a number from 1 to n. You have to guess which number I picked.
+//
+//		Every time you guess wrong, I'll tell you whether the number is higher or lower.
+//
+//		You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+//
+//		-1 : My number is lower
+//		 1 : My number is higher
+//		 0 : Congrats! You got it!
+//		Example:
+//		n = 10, I pick 6.
+//
+//		Return 6.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int guessNumber(int n) {
+		return bsearch(1,n);
+    }
+	
+	private int bsearch(int start, int end) {
+		
+		return 0;
+	}
+	public int guess(int num){
+		return 1;
+	}
+
+	
+
+}
