873b439ddab693892512e503284d30d684e4338e
Brooklynson31 <mikeharp212@gmail.com>
2017-02-03 08:26:05 -0500

    update

---
+++ EasyProblems/src/leetcode/stacks/MyStack.java
@@ -0,0 +1,50 @@
+package leetcode.stacks;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MyStack {
+	
+	
+	
+//	Implement the following operations of a stack using queues.
+//
+//	push(x) -- Push element x onto stack.
+//	pop() -- Removes the element on top of the stack.
+//	top() -- Get the top element.
+//	empty() -- Return whether the stack is empty.
+//	Notes:
+//	You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+//	Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
+//	You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		}
+		Deque<Integer> a;
+		public MyStack() {
+	        a = new ArrayDeque();
+	    }
+	    
+	    /** Push element x onto stack. */
+	    public void push(int x) {
+	        a.push(x);
+	    }
+	    
+	    /** Removes the element on top of the stack and returns that element. */
+	    public int pop() {
+	        return a.pop();
+	    }
+	    
+	    /** Get the top element. */
+	    public int top() {
+	    	return a.peek();
+	    }
+	    
+	    /** Returns whether the stack is empty. */
+	    public boolean empty() {
+	        return a.isEmpty();
+	    }
+
+}
