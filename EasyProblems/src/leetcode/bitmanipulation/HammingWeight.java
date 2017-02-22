b6e2403619a4ec066bc5712791234e8bf2e546c6
Brooklynson31 <mikeharp212@gmail.com>
2017-02-22 08:36:35 -0500

    update

--- EasyProblems/src/leetcode/bitmanipulation/HammingWeight.java
+++ EasyProblems/src/leetcode/bitmanipulation/HammingWeight.java
@@ -10,19 +10,21 @@ public class HammingWeight {
 		System.out.println(Integer.toBinaryString(n));
 		System.out.println(Integer.toBinaryString(n-1));
 		
-		System.out.println(Integer.bitCount(n));
+		//System.out.println(Integer.bitCount(n));
 
 		
-		//hammingWeight2(n);
-		System.out.println(hammingWeight(n));
+		hammingWeight2(n);
+		//System.out.println(hammingWeight(n));
 	}
 
 	// this method is a short cut because each operation will skip all bit places than contain 1
 	
 	public static int hammingWeight(int n) {
         int count = 0;
-        while(n != 0){ //will continue to sun until you are at 0 
-        	n = n & (n-1); // assigns n to an int that will have an int value that has all the bits toggled all the way up to the first 1 bit(will be changed to 0)
+        while(n != 0){ //will continue to run until you are at 0 
+        	n = n & (n-1);  //starting from right to left(bitwise AND) with n-1 will toggle all the bits until it arrives at a 1 in the bit
+
+        					// reassigns n to an int that will have one less 1 bit in it
         	count++; //counts how many times it runs this operation
         }
 		
@@ -30,16 +32,17 @@ public class HammingWeight {
     }
 	
 	public static void hammingWeight2(int n) {
-        Integer test = 1 << (Integer.SIZE/8 -1); //right most position in the check bit should be 1 at every iteration
-        while(test != 0){
-        	int result = n & test;
-        	if(result == test){
-        		System.out.println(1);
-        	} else {
-        		System.out.println(0);
-        	}
+        Integer checkbit = 1; //start with check bit(1) at the right most position
+        checkbit = checkbit << (Integer.SIZE/8 -1); //shifts the 1 in check bit so it is now at most significant position
+        //right most position in the check bit should be 1 at every iteration
+        int count = 0;
+        while(checkbit != 0){ //once the right most 1 is movted out to the left AND is  shifted out(to the right), then check bit will be 0
+        	int result = n & checkbit; //bitwise AND input int and right most bit
+        		if(result == checkbit)
+        			count++;
         	
-        	test = test >>1;
+        	checkbit = checkbit >>1; //keep shifting 1 in check bit ot the right until checkbit is 0
         }
+        System.out.println(count);
     }
 }
---
+++ EasyProblems/src/leetcode/bitmanipulation/ReverseBits.java
@@ -0,0 +1,52 @@
+package leetcode.bitmanipulation;
+
+public class ReverseBits {
+//	Reverse bits of a given 32 bits unsigned integer.
+//
+//	For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+//
+//	Follow up:
+//	If this function is called many times, how would you optimize it?
+
+	public static void main(String[] args) {
+
+		int n = 2678;
+		System.out.println(reverseBits2(n));
+		System.out.println(Integer.reverse(n));
+	}
+	
+	 public static	int reverseBits(int n) {
+	     int reverse = 0; //intialize the result
+	     long  count = (Integer.SIZE/8); //counter to track the number of bits in the integer --> allows us to short circuit out of reversal
+	    
+	     while(n != 0){
+	    	 int lastbit = n & 1; //extract the right most bit
+	    	 reverse = reverse | lastbit; //add it to the rightmost bit of the result
+	    	 reverse <<=1; //shift result left (insert next bit)
+	    	 n  >>= 1; //shift original number right(extract next bit)
+	    	 count --;
+	    	 
+	     }
+
+	     reverse = reverse << count; // if the original number has only 0's shift left the remaining bits
+		 
+		 return reverse;
+	    }
+	 
+	 public static int reverseBits2(int n){
+		 if( n == 0) return 0; //edge case
+		 
+		 
+		 int result = 0; //start result int at 0
+		 for(int i = 0; i < 32;i++){ // iterate through each possible bit in n
+			 result <<=1; //shift result bit over to left
+			 if((n & 1) == 1) //if bitwise & oepration with 1 as checkbit returns 1(then n has 1 at that bit place
+				 result++;  //increment bit at this position to 1 if n has 1 in this position.
+			 n >>=1; //shift n to the left by 1 to extract next bit
+		 }
+		 
+		 
+		 return result;
+	 }
+
+}
